e21803c8bf5f671d40b1a433cc5af3ce1959e446
Js <JimmyLoveSiren@users.noreply.github.com>
2020-03-19 11:37:30 +0900

    BigFoot v1.13.3.44

--- Interface/AddOns/BigFoot/Version.cn.lua
+++ Interface/AddOns/BigFoot/Version.cn.lua
@@ -1,6 +1,6 @@
 if GetLocale()~='zhCN' then return end
 local main= "1.13.3."
-local minor = "42"
+local minor = "44"
 BIGFOOT_VERSION = "zhCN"..main..minor;
 
 function GetMinorVersion()
@@ -8,6 +8,16 @@ function GetMinorVersion()
 end
 
 BigFootChangelog_ah();
+
+	BigFootChangelog_at("2020/03/13 (1.13.3.44)")
+	BigFootChangelog_ar("大脚插件","版本号同步至游戏版本。")
+	BigFootChangelog_ar("大脚插件","同步更新整合类插件。")
+	BigFootChangelog_ar("密语管理","修复一个已知问题。")
+
+
+	BigFootChangelog_at("2020/03/11 (1.13.3.43)")
+	BigFootChangelog_ar("大脚插件","同步更新整合类插件。")
+
 
 	BigFootChangelog_at("2020/02/27 (1.13.3.42)")
 	BigFootChangelog_ar("大脚插件","同步更新整合类插件。")
@@ -231,4 +241,4 @@ BigFootChangelog_ah();
 BigFootChangelog_af();
 
 
-BF_VERSION_CHECKSUM = "55"
+BF_VERSION_CHECKSUM = "49"
